2f80f98ee6287ea8c6cc56fc52fc00b9d368d99e
Ivan Talantov <www.vankj@mail.ru>
2023-02-04 19:25:48 +0700

    feat: new page

---
+++ src/pages/CalculatePage/CalculatePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import styles from "./CalculatePage.module.css";
+import { CalcMatrixForm } from "../../components/CalcMatrixForm/CalcMatrixForm";
+
+export const CalculatePage = () => {
+  return (
+    <div className={styles.calcPage}>
+      <Header />
+      <CalcMatrixForm />
+      <Footer />
+    </div>
+  );
+};
